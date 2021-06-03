5dc7719d69a082fbfd63573958a04983eac0ab74
janstenpickle <chris.jansen@permutive.com>
2021-06-03 17:40:06 +0100

    Remove unused deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,10 +8,7 @@ object Dependencies {
     val trace4cats = "0.12.0-RC1+146-d193db1e"
 
     val circe = "0.14.1"
-    val grpc = "1.38.0"
     val http4s = "0.23.0-RC1"
-    val openTelemetry = "1.2.0"
-    val scalapb = "0.11.0"
   }
 
   lazy val trace4catsExporterCommon = "io.janstenpickle" %% "trace4cats-exporter-common" % Versions.trace4cats
@@ -21,13 +18,6 @@ object Dependencies {
     "io.janstenpickle"                          %% "trace4cats-jaeger-integration-test" % Versions.trace4cats
   lazy val trace4catsModel = "io.janstenpickle" %% "trace4cats-model"                   % Versions.trace4cats
 
-  lazy val circeGeneric = "io.circe"                       %% "circe-generic-extras"          % Versions.circe
-  lazy val grpcApi = "io.grpc"                              % "grpc-api"                      % Versions.grpc
-  lazy val grpcOkHttp = "io.grpc"                           % "grpc-okhttp"                   % Versions.grpc
-  lazy val http4sBlazeClient = "org.http4s"                %% "http4s-blaze-client"           % Versions.http4s
-  lazy val openTelemetrySdk = "io.opentelemetry"            % "opentelemetry-sdk"             % Versions.openTelemetry
-  lazy val openTelemetryOtlpExporter = "io.opentelemetry"   % "opentelemetry-exporter-otlp"   % Versions.openTelemetry
-  lazy val openTelemetryJaegerExporter = "io.opentelemetry" % "opentelemetry-exporter-jaeger" % Versions.openTelemetry
-  lazy val openTelemetryProto = "io.opentelemetry"          % "opentelemetry-proto"           % Versions.openTelemetry.concat("-alpha")
-  lazy val scalapbJson = "com.thesamet.scalapb"            %% "scalapb-json4s"                % Versions.scalapb
+  lazy val circeGeneric = "io.circe"        %% "circe-generic-extras" % Versions.circe
+  lazy val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client"  % Versions.http4s
 }
