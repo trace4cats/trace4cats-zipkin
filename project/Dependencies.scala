7abc204a9390c9f71282471e2f5d794e777115d3
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-08-06 07:58:49 +0300

    Prepare release 0.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,12 +6,12 @@ object Dependencies {
     val scala213 = "2.13.6"
     val scala3 = "3.0.1"
 
-    val trace4cats = "0.12.0-RC2+17-d73c7ff3"
-    val trace4catsExporterHttp = "0.12.0-RC2+4-69bfcea6"
-    val trace4catsJaegerIntegrationTest = "0.12.0-RC2+7-e4062471"
+    val trace4cats = "0.12.0"
+    val trace4catsExporterHttp = "0.12.0"
+    val trace4catsJaegerIntegrationTest = "0.12.0"
 
     val circe = "0.14.1"
-    val http4s = "0.23.0-RC1"
+    val http4s = "0.23.0"
 
     val kindProjector = "0.13.0"
     val betterMonadicFor = "0.3.1"
